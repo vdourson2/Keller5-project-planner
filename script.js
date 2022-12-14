6536b7117039d788db921e5c8386fa4e560f32b4
Virginie Dourson <virginiedourson@gmail.com>
2022-12-14 16:56:21 +0100

    Initialisation de tasks de base

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
+//Encodage manuel de quelques cartes pour l'exercice
+const base = [
+    {
+        name : "Initialisation",
+        description : "Démarrer le processus et créer les documents de base",
+        due : "2022-12-28",
+        label : "done"
+    },
+    {
+        name : "Répartition des tâches",
+        description : "Déterminer les différentes tâches à réaliser et dispatcher suivant les souhaits et les compétences",
+        due : "2023-01-15",
+        label : "doing" 
+    },
+    {
+        name : "Contact avec le client",
+        description : "Fixer un rendez-vous avec Monsieur Tartenpiont et voir avec lui les exigences au niveau du design",
+        due : "2023-01-15",
+        label : "doing"
+    },
+    {
+        name : "Présentation de l'avant-projet",
+        description : "Fixer une date et réunir toute l'équipe pour la présentation de l'avant-projet",
+        due : "2023-02-10",
+        label : "to-do"
+    },
+]
+for (let b of base){
+    create_task(b.name, b.description, b.due, b.label)
+}
+
 //récupérer les informations du formulaire
 const recup = (e) => {
+    //Récupération des données du formulaire
     let nameOr = document.getElementById('form__name').value;
     let descriptionOr = document.getElementById('form__description').value;
     let dueOr = document.getElementById('form__due').value;
+    console.log(dueOr);
     let labelOr = document.getElementById('form__label').value;
+    console.log(labelOr);
+    //Reset des zones d'encodage
     document.getElementById("formulaire").reset();
+    //Appel de la fonction de création des cartes dans la div "task."
     create_task(nameOr, descriptionOr, dueOr, labelOr);
+    
+    /*Local storage
     let tache = {
         name : nameOr,
         description : descriptionOr,
@@ -15,12 +53,13 @@ const recup = (e) => {
     liste.push(tache);
     console.log(liste);
     localStorage.setItem("Liste",JSON.stringify(liste));
-    //localStorage.clear();
+    localStorage.clear();*/
 
 }
 
 let submit = document.getElementById("submit");
-let liste = [];
+/*local storage;
+let liste = [];*/
 submit.addEventListener('click', recup);
 
 
